585de335495a0c74cf70be79e7970df0dedec768
Alexandre Costa <alexandre@brim.com.br>
2023-09-26 08:51:15 +0100

    first commit

---
+++ app/(dashboard)/projects/list/page.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import styles from './projects.module.css';
+
+
+export default function page() {
+	return (
+		<main>
+			<h1>Project List</h1>
+			<ul className={styles.ul}>
+				<li>
+					<Link href='/projects/jobit'>JobIt</Link>
+				</li>
+				<li>
+					<Link href='/projects/carrent'>Car Rent</Link>
+				</li>
+				<li>
+					<Link href='/projects/hipnode'>HipNode</Link>
+				</li>
+			</ul>
+		</main>
+	);
+}
